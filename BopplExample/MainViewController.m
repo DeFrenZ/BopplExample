3ae0b61bf3bd4f60cd1b460ddd97fa0b4af3bce3
Davide De Franceschi <DeFrenZ@defroBook-Pro.local>
2014-08-20 16:46:21 +0100

    Initial Commit.

--- BopplExample/MainViewController.m
+++ BopplExample/MainViewController.m
@@ -6,13 +6,13 @@
 //  Copyright (c) 2014 Davide De Franceschi. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "MainViewController.h"
 
-@interface ViewController ()
+@interface MainViewController ()
 
 @end
 
-@implementation ViewController
+@implementation MainViewController
 
 - (void)viewDidLoad
 {
